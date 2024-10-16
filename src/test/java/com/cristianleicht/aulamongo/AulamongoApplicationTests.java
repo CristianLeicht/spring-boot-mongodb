ad02c3880b252488a6e893ecc2d13bd5c59e3ca6
Cristian Leicht <cristian.leicht@hotmail.com>
2024-10-16 20:25:57 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/cristianleicht/aulamongo/AulamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianleicht.aulamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
